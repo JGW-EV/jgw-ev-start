01f544f14defe28c74e5761726486d36b64aa90a
ySimonGit <simon.sch3112@gmail.com>
2021-07-30 17:06:44 +0200

    Added some tiles
    - Added interiour tileset
    - Added nature tileset

---
+++ src/assets/interiour.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="interiour" tilewidth="32" tileheight="32" tilecount="3248" columns="58">
+ <image source="interiour.png" width="1886" height="1792"/>
+</tileset>
