642810b2fcb8c0d69d933d04247077e26cbc363e
ySimonGit <simon.sch3112@gmail.com>
2021-07-31 13:11:05 +0200

    Fixed a map problem (override)

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ let currentPopup: any = undefined;
 
 // Credits
 WA.onEnterZone('credits', () => {
-    currentPopup = WA.openPopup("creditsPopupHeader", "Map-Ersteller:", []);
-    currentPopup = WA.openPopup("creditsPopup", "Vinzenz & Simon", []);
-    currentPopup = WA.openPopup("creditsPopup", "Vielen Dank an Christoph", []);
+    currentPopup = WA.openPopup("creditsPopupHead", "Map-Ersteller:", []);
+    currentPopup = WA.openPopup("creditsPopupBody", "Vinzenz & Simon", []);
+    currentPopup = WA.openPopup("creditsPopupFooter", "Vielen Dank an Christoph", []);
     console.log(document.getElementById('container'));
 });
 
