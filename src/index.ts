2e7ff46b3904018e992c065246863a5b7342cf8e
ySimonGit <simon.sch3112@gmail.com>
2021-07-31 17:00:29 +0200

    Changed the popups

--- src/index.ts
+++ src/index.ts
@@ -12,42 +12,42 @@ WA.onLeaveZone('credits', closePopUp);*/
 
 // Popups
 WA.onEnterZone('meet1_top', () => {
-    currentPopup = WA.openPopup("meet1_top", "Kursraum 1", []);
+    currentPopup = WA.openPopup("meet1_top_pop", "Kursraum 1", []);
 });
 WA.onLeaveZone('meet1_left', closePopUp);
 
 WA.onEnterZone('meet1_left', () => {
-    currentPopup = WA.openPopup("meet1_left", "Kursraum 1", []);
+    currentPopup = WA.openPopup("meet1_left_pop", "Kursraum 1", []);
 });
 WA.onLeaveZone('meet1_left', closePopUp);
 
 WA.onEnterZone('meet2', () => {
-    currentPopup = WA.openPopup("meet2", "Kursraum 2", []);
+    currentPopup = WA.openPopup("meet2_pop", "Kursraum 2", []);
 });
 WA.onLeaveZone('meet2', closePopUp);
 
 WA.onEnterZone('meet3_top', () => {
-    currentPopup = WA.openPopup("meet3_top", "Kursraum 3", []);
+    currentPopup = WA.openPopup("meet3_top_pop", "Kursraum 3", []);
 });
 WA.onLeaveZone('meet3_top', closePopUp);
 
 WA.onEnterZone('meet3_right', () => {
-    currentPopup = WA.openPopup("meet3_right", "Kursraum 3", []);
+    currentPopup = WA.openPopup("meet3_right_pop", "Kursraum 3", []);
 });
 WA.onLeaveZone('meet3_right', closePopUp);
 
 WA.onEnterZone('meet4_right', () => {
-    currentPopup = WA.openPopup("meet4_right", "Kursraum 4", []);
+    currentPopup = WA.openPopup("meet4_right_pop", "Kursraum 4", []);
 });
 WA.onLeaveZone('meet4_right', closePopUp);
 
 WA.onEnterZone('meet4_left', () => {
-    currentPopup = WA.openPopup("meet4_left", "Kursraum 4", []);
+    currentPopup = WA.openPopup("meet4_left_pop", "Kursraum 4", []);
 });
 WA.onLeaveZone('meet4_left', closePopUp);
 
 WA.onEnterZone('meet5', () => {
-    currentPopup = WA.openPopup("meet5", "Kursraum 5", []);
+    currentPopup = WA.openPopup("meet5_pop", "Kursraum 5", []);
 });
 WA.onLeaveZone('meet5', closePopUp);
 
